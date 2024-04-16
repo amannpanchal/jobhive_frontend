60617cc9b00674a715950aeaaed876691f31e2d3
Aman <128085412+amannpanchal@users.noreply.github.com>
2024-04-16 23:03:31 +0530

    Update config.js

--- src/URLS/config.js
+++ src/URLS/config.js
@@ -1,6 +1,6 @@
 import { extendTheme } from "@chakra-ui/react";
 
-export const MAIN_URL = 'http://localhost:4000/api/v1'
+export const MAIN_URL = 'https://jobhive-backend.onrender.com/api/v1'
 // export const theme = extendTheme({
 //   config: {
 //     initialColorMode: "dark",
