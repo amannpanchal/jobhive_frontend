30a3d47c4c9035b7caf79f19ca726d76c42563ac
Aman <128085412+amannpanchal@users.noreply.github.com>
2024-12-01 12:13:17 +0530

    Update config.js

--- src/URLS/config.js
+++ src/URLS/config.js
@@ -1,6 +1,6 @@
 import { extendTheme } from "@chakra-ui/react";
 
-export const MAIN_URL = 'https://jobhive-backend.onrender.com/api/v1'
+export const MAIN_URL = 'https://hiveback.onrender.com/api/v1'
 // export const theme = extendTheme({
 //   config: {
 //     initialColorMode: "dark",
